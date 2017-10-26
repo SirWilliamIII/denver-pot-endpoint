aa695077a8f72cf8ee954db39663c6128105760e
Will Carpenter <willcarpenteriii@gmail.com>
2017-10-26 13:48:34 -0600

    changed knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: "pg",
-    connection: "postgres://localhost/strains"
+    connection: 'postgres://localhost/strains'
   },
   production: {
     client: "pg",
